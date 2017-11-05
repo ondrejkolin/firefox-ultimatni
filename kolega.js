b4b8617cde576cc33e69a3239aa47bc68bca3299
Michal Vašíček <michal.vasicek@moderntv.eu>
2017-11-05 13:12:35 +0100

    Colors

--- kolega.js
+++ kolega.js
@@ -1,7 +1,25 @@
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+const els = document.querySelectorAll("body *");
+
+const colorInterval = setInterval(() => {
+    let prvek = els[Math.floor(Math.random() * els.length)];
+    prvek.style = 'background-color:' + getRandomColor();
+    console.log('color', prvek);
+}, 1);
+
 const interval = setInterval(() => {
     const prvky = document.querySelectorAll("body *");
     if (!prvky.length) {
         clearInterval(interval);
+        clearInterval(colorInterval);
         const body = document.querySelectorAll("body")[0];
         body.className = 'warning';
         setTimeout(function () {
