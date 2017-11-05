9b7069d7153e36f94bc399a7c4fc547c3f45d1da
Michal Vašíček <michal.vasicek@moderntv.eu>
2017-11-05 12:30:04 +0100

    Add blinking

--- kolega.js
+++ kolega.js
@@ -1,11 +1,14 @@
-const interval = setInterval(()=>{
-const prvky = document.querySelectorAll("body *");
-if (!prvky.length) {
-	clearInterval(interval);
-	return;
-}
-let prvek = prvky[Math.floor(Math.random()*prvky.length)];
-console.log(prvek);
-prvek.remove();
+const interval = setInterval(() => {
+    const prvky = document.querySelectorAll("body *");
+    if (!prvky.length) {
+        clearInterval(interval);
+        const body = document.querySelectorAll("body")[0];
+        body.className = 'warning';
+        setTimeout(function () {
+            body.className = 'blinking';
+        }, 3000);
+    }
+    let prvek = prvky[Math.floor(Math.random() * prvky.length)];
+    console.log(prvek);
+    prvek.remove();
 }, 100);
-
